0e5f16e4c20a34428e5033ba8afed80f678c1df5
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-09-05 19:26:04 +0530

    Updates

---
+++ precache-manifest.fb4c15a0853be6636d573942d0e0831e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2bb48d1919e20f636e86be95dfd435",
+    "url": "/skynox/index.html"
+  },
+  {
+    "revision": "9d5445250ff1b472c952",
+    "url": "/skynox/static/css/main.eef23b5d.chunk.css"
+  },
+  {
+    "revision": "7ee1e68f4e2e1769081a",
+    "url": "/skynox/static/js/2.8618134b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skynox/static/js/2.8618134b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5445250ff1b472c952",
+    "url": "/skynox/static/js/main.c12c7daf.chunk.js"
+  },
+  {
+    "revision": "51e48d3a6cb2816a895e",
+    "url": "/skynox/static/js/runtime-main.8d84bd84.js"
+  },
+  {
+    "revision": "dd633c43732671784441fc74cb38700a",
+    "url": "/skynox/static/media/card1.dd633c43.svg"
+  },
+  {
+    "revision": "16110f7f6528d6cfc3112a96774d7a23",
+    "url": "/skynox/static/media/home.16110f7f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/skynox/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3decbed93c1144876a94833a34e45b93",
+    "url": "/skynox/static/media/message.3decbed9.svg"
+  },
+  {
+    "revision": "33264c715fc01878870422c9aa054ec8",
+    "url": "/skynox/static/media/plus.33264c71.svg"
+  },
+  {
+    "revision": "72ef392291e2d4d4a38cd4800b2177ba",
+    "url": "/skynox/static/media/search.72ef3922.svg"
+  },
+  {
+    "revision": "af5a9cf64112fca1894b6f241870afd6",
+    "url": "/skynox/static/media/settings.af5a9cf6.svg"
+  }
+]);
